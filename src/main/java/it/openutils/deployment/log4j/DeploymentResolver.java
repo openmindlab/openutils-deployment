c4e6137d6fd7586122c7e64044ff7ccf9c9b44da
fabrizio.giustina <fabrizio.giustina@openmindonline.it>
2008-02-03 21:20:09 +0000

    [maven-release-plugin]  copy for tag openutils-deployment-2.0

--- src/main/java/it/openutils/deployment/log4j/DeploymentResolver.java
+++ src/main/java/it/openutils/deployment/log4j/DeploymentResolver.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright Openmind http://www.openmindonline.it
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package it.openutils.deployment.log4j;
 
 import java.io.File;
@@ -50,7 +65,7 @@ public class DeploymentResolver
             MessageFormat
                 .format(
                     "No configuration found using location list {0}. [servername] is [{1}], [webapp] is [{2}] and base path is [{3}]", //$NON-NLS-1$
-                    new Object[]{ArrayUtils.toString(propertiesLocation), servername, webapp, rootPath }));
+                    new Object[]{ArrayUtils.toString(propertiesLocation), servername, webapp, rootPath}));
 
     }
 
